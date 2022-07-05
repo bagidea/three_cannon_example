192658c40ace1e885d31bcf50e4e601a243f2390
bagidea <thanawat@nexhe.com>
2022-07-06 03:53:24 +0700

    add block 32x32 and setting camera and controls

--- src/components/webgl/minecraft_components/index.tsx
+++ src/components/webgl/minecraft_components/index.tsx
@@ -1,9 +1,13 @@
 import {
     BoxGeometry,
+    EdgesGeometry,
+    LineBasicMaterial,
+    LineSegments,
     Mesh,
     MeshBasicMaterial,
     PerspectiveCamera,
     Scene,
+    Vector3,
     WebGLRenderer
 } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
@@ -46,11 +50,13 @@ export class MinecraftExample {
 
         this.scene = new Scene()
 
-        this.camera = new PerspectiveCamera(60, this.w/this.h, 1, 100)
-        this.camera.position.set(0, 2, 5)
-        this.camera.lookAt(0, 0, 0)
+        this.camera = new PerspectiveCamera(60, this.w/this.h, 0.1, 1000)
+        this.camera.position.set(16, 10, 40)
+        this.camera.lookAt(16, 0, 16)
 
         this.controls = new OrbitControls(this.camera, this.canvas)
+        this.controls.target = new Vector3(16, 0, 16)
+        this.controls.update()
 
         window.addEventListener('resize', this.windowResize)
     }
@@ -58,9 +64,27 @@ export class MinecraftExample {
     create() {
         const boxGeometry: BoxGeometry = new BoxGeometry(1, 1, 1)
         const boxMaterial: MeshBasicMaterial = new MeshBasicMaterial({ color: 0xffffff })
-        const box: Mesh = new Mesh(boxGeometry, boxMaterial)
 
-        this.scene.add(box)
+        //const box: Mesh = new Mesh(boxGeometry, boxMaterial)
+        //this.scene.add(box)
+
+        const edgesGeometry: EdgesGeometry = new EdgesGeometry(boxGeometry)
+        const edgesMaterial: LineBasicMaterial = new LineBasicMaterial({ color: 0x000000 })
+
+        //const line: LineSegments = new LineSegments(edgesGeometry, edgesMaterial)
+        //this.scene.add(line)
+
+        for(let i: number = 0; i < 32; i++) {
+            for(let a: number = 0; a < 32; a++) {
+                const box: Mesh = new Mesh(boxGeometry, boxMaterial)
+                box.position.set(i, 0, a)
+                this.scene.add(box)
+
+                const line: LineSegments = new LineSegments(edgesGeometry, edgesMaterial)
+                line.position.copy(box.position)
+                this.scene.add(line)
+            }
+        }
     }
 
     render = () => {
